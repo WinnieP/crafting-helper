d0bef25b5c7207ff2157630b44294d64744005c1
Winston Huang <wh5480@gmail.com>
2013-06-16 22:19:18 -0700

    refactoring

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
+// This probably isn't a good idea
+var TaskNames = {
+	platesmithing: {
+		gatherHighQualityOre: 'Gather High quality Iron Ore',
+		forgeSteelPlates: 'Forge Steel Plates'
+	}
+};
+
 function main() {
-	gatherHighQualityIronOre();
+	processChain([
+		Tasks.collect(1),
+		Timing.pause,
+		Tasks.startTask('platesmithing', TaskNames.platesmithing.gatherHighQualityOre)
+	]);
 }
 
-function processChain() {
+function processChain(steps) {
 	var chain = $.Deferred(),
 		lastRet = chain,
 		arg;
 
-	for (var i = 0; i < arguments.length; i++) {
-		arg = arguments[i];
+	for (var i = 0; i < steps.length; i++) {
+		arg = steps[i];
 		// bleh - find easy way to call function like this w/o having arg be of 2 types
 		if (arg instanceof Array) {
 			lastRet = lastRet.then(arg[0], arg[1]);
@@ -18,25 +30,36 @@ function processChain() {
 	}
 
 	chain.resolve();
+	return lastRet;
 }
 
-var DEFAULT_WAIT = 3000;
+function createStep() {
+	var args = arguments;
+
+	return function() {
+		var d = $.Deferred();
+
+		processChain(args).then(d.resolve, d.reject)
+
+		return d.promise();
+	}
+}
+
+function clickElement($element) {
+	var event = document.createEvent("MouseEvent");
+	event.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+	$element[0].dispatchEvent(event);
+}
 
 var NavigateTo = (function() {
 
 	var dataUrls = {
-		professions: '/professions',
+		overview: '/professions',
 		platesmithing: '/professions-tasks/Armorsmithing_Heavy'
 	};
 
-	function clickElement($element) {
-		var event = document.createEvent("MouseEvent");
-		event.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-		$element[0].dispatchEvent(event);
-	}
-
 	function findByDataUrl(dataUrl) {
-		return $('a[data-url="' + dataUrl + '"]');
+		return $('a:visible[data-url="' + dataUrl + '"]'); // visible b/c not sure what that other thing is
 	}
 
 	function findTask(title) {
@@ -44,9 +67,6 @@ var NavigateTo = (function() {
 		return $title.closest('tr'); // brittleness alert
 	}
 
-	function continueTask($task) {
-		clickElement($task.find('button')); // brittleness alert
-	}
 
 	function nextPage() {
 		clickElement($('#tasklist_next'));
@@ -54,30 +74,17 @@ var NavigateTo = (function() {
 
 	var navigateTo = {
 
-		professions: function() {
-			clickElement(findByDataUrl(dataUrls.professions));
+		overview: function() {
+			clickElement(findByDataUrl(dataUrls.overview));
 		},
 
 		platesmithing: function() {
 			clickElement(findByDataUrl(dataUrls.platesmithing));
 		},
 
-		task: function(taskTitle) {
-			// When should we start worrying?
-			return function() {
-				var d = $.Deferred();
-
-				processChain(
-					navigateTo.pageWithTask(taskTitle, 20),
-					continueTask
-				);
-
-				return d.promise();
-			};
-		},
-
 		// Returns callback that will go to next page until task is found
 		// and resolve with the jQuery row entry corresponding to that task
+		// Probably better to just keep going until on last page and then reject	
 		pageWithTask: function(taskTitle, pagesToTry) {
 			return function() {
 				var d = $.Deferred(),
@@ -89,12 +96,12 @@ var NavigateTo = (function() {
 					if (task.length) {
 						d.resolve(task);
 					} else {
-						processChain(
+						processChain([
 							nextPage,
 							Timing.pause,
 							navigateTo.pageWithTask(taskTitle, pagesToTry-1),
 							[d.resolve, d.reject]
-						);
+						]);
 					}
 				}
 
@@ -102,9 +109,72 @@ var NavigateTo = (function() {
 				return d.promise();
 			}
 		}
+
 	};
 
 	return navigateTo;
+
+})();
+
+var Tasks = (function() {
+
+	function clickContinue($task) {
+		clickElement($task.find('button')); // brittleness alert
+	}
+
+	function clickStart() {
+		clickElement($('button:contains("Start Task")'));
+	}
+
+	function collectSlot(slot) {
+		return function() {
+			clickElement($('.professions-slots li:nth-child(' + (slot + 1) + ') button:contains("Collect Result")'));
+		}
+	}
+
+	function collectModal() {
+		clickElement($('#modal_content button:contains("Collect Result")'));
+	}
+	
+	function openTask(taskTitle) {
+		// When should we start worrying?
+		return function() {
+			var d = $.Deferred();
+
+			processChain([
+				NavigateTo.pageWithTask(taskTitle, 20),
+				clickContinue,
+				Timing.pause,
+				[d.resolve, d.reject]
+			]);
+
+			return d.promise();
+		};
+	}
+
+	var tasks = {
+
+		collect: function(slot) {
+			return createStep(
+				NavigateTo.overview,
+				Timing.pause,
+				collectSlot(slot),
+				Timing.pause,
+				collectModal
+			);
+		},
+
+		startTask: function(profession, taskName) {
+			return createStep(
+				NavigateTo[profession],
+				Timing.pause,
+				openTask(taskName),
+				clickStart
+			);
+		}
+	};
+
+	return tasks;
 
 })();
 
@@ -114,11 +184,14 @@ var Timing = (function() {
 	var timing = {
 
 		wait: function(milliseconds) {
-			return function() {
+			return function(val) {
 				var waitForIt = $.Deferred();
 
 				setTimeout(function() {
-					waitForIt.resolve();
+					// won't really do what we want if being called as a reject callback,
+					// but will that happen? Any way to know what happened to previous callback in chain?
+					// reject always called with an error object?
+					waitForIt.resolve(val);
 				}, milliseconds)
 
 				return waitForIt.promise();
@@ -127,7 +200,10 @@ var Timing = (function() {
 	}
 
 	// have this rely on waiting for that spinny thing to go away? $('.loading-block').length
-	timing.pause = timing.wait(3000);
+	timing.pause = timing.wait(5000);
+
+	// for non-server call things? better name needed
+	timing.moment = timing.wait(500);
 
 	return timing;
 })();
@@ -137,13 +213,15 @@ var Logging = (function() {
 	var logging = {
 
 		console: function(message) {
-			return function() {
+			return function(val) {
 				console.log(message);
+				return $.Deferred().resolve(val).promise();
 			}
 		},
 		alert: function(message) {
-			return function() {
+			return function(val) {
 				alert(message);
+				return $.Deferred().resolve(val).promise();
 			}
 		}
 	};
@@ -151,22 +229,5 @@ var Logging = (function() {
 	return logging;
 })();
 
-var Tasks = {
-	gather: {
-		ore: {
-			high: 'Gather High quality Iron Ore'
-		}
-	}
-};
-
-// make a mapping table of profession -> known tasks and have it auto navigate by passing in string/const?
-function gatherHighQualityIronOre() {
-	processChain(
-		NavigateTo.platesmithing,
-		Timing.pause,
-		NavigateTo.task(Tasks.gather.ore.high),
-		[Logging.alert('found'), Logging.alert('not found')]
-	);
-}
 
 main();
